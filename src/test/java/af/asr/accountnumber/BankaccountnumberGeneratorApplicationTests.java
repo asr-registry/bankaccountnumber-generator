9ebc1ea269b174167bcd101e35ef7a7669df3242
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-20 16:16:52 +0430

    bank account generator library

---
+++ src/test/java/af/asr/accountnumber/BankaccountnumberGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.accountnumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaccountnumberGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
